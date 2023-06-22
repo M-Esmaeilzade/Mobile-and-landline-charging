3d8e0a5f28e400aeed6c30f008c297d7c3afca4f
M-Esmaeilzade <mesmaeilzade653@gmail.com>
2023-06-22 15:38:26 -0700

    SplashActivity.java and MainActivity.java finish

--- app/src/main/java/com/example/mobile_and_landing_charging/MainActivity.java
+++ app/src/main/java/com/example/mobile_and_landing_charging/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.mobile_and_landing_charging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.mobile_and_landing_charging.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, ChargeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.btnPayBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, LandlineActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
